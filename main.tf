c2bb47a822c5389e8391111baaa3b30b96aa51c7
venky1509 <136893052+venky1509@users.noreply.github.com>
2024-07-31 05:41:15 +0000

    C2

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 provider "aws" {
-region="us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region = "us-east-1"
 }
+
+variable "access_key" {
+    type=string
+}
+
+variable "secret_key" {
+    type=string
+}
+
 
 resource "aws_vpc" "test1" {
     cidr_block = "192.168.0.0/16"
