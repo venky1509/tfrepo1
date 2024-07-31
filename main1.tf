8fcfddf7956687d51b3b7d11c3c66bf8b2c5ab5b
venky1509 <136893052+venky1509@users.noreply.github.com>
2024-07-31 09:10:21 +0000

    C2

--- main1.tf
+++ main1.tf
@@ -18,11 +18,11 @@ algorithm = "RSA"
 rsa_bits = 4096
 
 }
-resource "aws_key_pair" "tf-key-pair" {
+resource "aws_key_pair" "tfec2-key-pair" {
 key_name = var.keyname1
 public_key = tls_private_key.rsa.public_key_openssh
 }
-resource "local_file" "tf-key" {
+resource "local_file" "tfec2-key" {
 
 content = tls_private_key.rsa.private_key_pem
 
@@ -30,7 +30,7 @@ filename = var.keyname
 
 }
 
-resource "aws_instance" "web-server" {
+resource "aws_instance" "web-server26" {
  ami      = "ami-0a0e5d9c7acc336f1"
  instance_type = "t2.micro"
  key_name   = var.keyname1
