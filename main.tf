1a2d8b066225406528e024431fb9bd182d10d6af
venky1509 <136893052+venky1509@users.noreply.github.com>
2024-07-31 06:12:00 +0000

    C4

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_vpc" "test1" {
-    cidr_block = "192.168.0.0/16"
+    cidr_block = "192.165.0.0/16"
     tags = {
 
       "Name" = "VPC1"
